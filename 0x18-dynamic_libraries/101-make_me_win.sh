3f44f8d332f3b1c42c76d375d5b5f5216ad0eaf6
zebbyG <99161685+zebbyG@users.noreply.github.com>
2022-06-14 15:54:09 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/KipronohVincent/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/zebbyG/alx-low_level_programming/tree/master/0x18-dynamic_libraries
 export LD_PRELOAD=/tmp/libmask.so

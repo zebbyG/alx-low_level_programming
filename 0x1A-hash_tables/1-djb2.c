ec1a55d5f226135aceb7719c5bdd60c8e07c053d
email <zebbyG>
2022-06-30 20:38:19 +0300

    fix task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: string
+**/
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
